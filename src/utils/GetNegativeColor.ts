82961aa0f1a8c401ae7fa55486a367911db1a005
José Trovarelli Neto <77056527+Trovarelli@users.noreply.github.com>
2025-08-22 07:09:04 -0300

    test: cover GetNegativeColor

--- src/utils/GetNegativeColor.ts
+++ src/utils/GetNegativeColor.ts
@@ -1,3 +1,7 @@
-export const GetNegativeColor = (color: 'white' | 'black' | null) => {
-    return color === 'white' ? 'black' : 'white'
+export const GetNegativeColor = (
+  color: 'white' | 'black' | null,
+): 'white' | 'black' | null => {
+  if (color === 'white') return 'black'
+  if (color === 'black') return 'white'
+  return null
 }
